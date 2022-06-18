50f18a207b3626a2b3b92e0cb8f763da2103fcab
Grimlerih <61974670+Grimlerih@users.noreply.github.com>
2022-06-18 21:48:33 +0300

    возможность редактирования профиля и  добавления карточек

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,65 @@
 let editButton = document.querySelector('.profile__button-edit');
 let popupEdit = document.querySelector('.popup');
-let popupContainerEdit = document.querySelector('.popup__container_edit');
-let popupContainerAdd = document.querySelector('.popup__container_add');
-let closeEditButton = document.querySelector('.popup__button-close');
+let closeButtonAdd = document.getElementById('add_form_close');
+let closeButtonEdit = document.getElementById('edit_form_close');
 let addButton = document.querySelector('.profile__button-add');
+let popupEditForm = document.querySelector('#popup__edit-container');
+let popupAddForm = document.querySelector('#popup__add-container');
 
 //открытие модального окна редактирования профиля
 editButton.addEventListener('click', function () {
   popupEdit.classList.add('popup_opened');
-  popupContainerEdit.classList.add('popup__container_opened');
+  popupEditForm.classList.remove('popup__container_type_edit');
 });
 
 //открытие модального окна добавления карточки
 addButton.addEventListener('click', function () {
   popupEdit.classList.add('popup_opened');
-  popupContainerAdd.classList.add('popup__container_opened');
+  popupAddForm.classList.remove('popup__container_type_add');
 });
 
-//закрытие модального окна редактирования профиля
-closeEditButton.addEventListener('click', function () {
+//закрытие модального окна добавления карточки и редактирования профиля
+function closeForm() {
   popupEdit.classList.remove('popup_opened');
-  popupContainerEdit.classList.remove('popup__container_opened');
-  popupContainerAdd.classList.remove('popup__container_opened');
-});
+  popupAddForm.classList.add('popup__container_type_add');
+  popupEditForm.classList.add('popup__container_type_edit');
+}
+
+closeButtonAdd.addEventListener('click', closeForm);
+closeButtonEdit.addEventListener('click', closeForm);
 
 //редактирование информации имени и работы
-const nameInput = document.querySelector('.form__text_type_name');
-const jobInput = document.querySelector('.form__text_type_activity');
-let nameText = document.querySelector('.profile__name');
-let jobText = document.querySelector('.profile__activity');
-const submitButton = document.querySelector('.form__button');
+const submitButtonEdit = document.getElementById('form__button_edit');
+const EditForm = document.querySelector('#edit__form');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
+  const nameInput = document.querySelector('.form__text_type_name');
+  const jobInput = document.querySelector('.form__text_type_activity');
+  let nameText = document.querySelector('.profile__name');
+  let jobText = document.querySelector('.profile__activity');
   nameText.textContent = nameInput.value;
   jobText.textContent = jobInput.value;
-  popupEdit.classList.toggle('popup_opened');
+  closeForm();
 };
 
-submitButton.addEventListener('click', formSubmitHandler);
+EditForm.addEventListener('submit', formSubmitHandler);
 
 //добавление карточек методом template
-const placeTemplate = document.querySelector('#place').content;
+const submitButtonAdd = document.querySelector('#form__button_add');
 const placeContainer = document.querySelector('.elements');
-const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+const addForm = document.querySelector('#add__form');
+const placeName = document.querySelector('.form__text_place-name');
+const placeUrl = document.querySelector('.form__url');
 
-placeTemplate.querySelector('.place__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
-placeTemplate.querySelector('.place__name').textContent = 'Архыз';
-songsContainer.append(songElement);
+function addCards(event) {
+  event.preventDefault();
+  const placeTemplate = document.querySelector('#place').content;
+  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+  placeElement.querySelector('.place__name').textContent = placeName.value;
+  placeElement.querySelector('.place__image').src = placeUrl.value;
+  placeContainer.prepend(placeElement);
+  closeForm();
+};
+
+addForm.addEventListener('submit', addCards);
