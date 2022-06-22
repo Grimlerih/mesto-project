53f100dbbc72444bb9dc1ca64b54053c279623a7
Grimlerih <61974670+Grimlerih@users.noreply.github.com>
2022-06-22 20:13:02 +0300

    добавлен функционал лайка и удаления карточек

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,19 @@
-let editButton = document.querySelector('.profile__button-edit');
-let popupEdit = document.querySelector('.popup');
-let closeButtonAdd = document.getElementById('add_form_close');
-let closeButtonEdit = document.getElementById('edit_form_close');
-let addButton = document.querySelector('.profile__button-add');
-let popupEditForm = document.querySelector('#popup__edit-container');
-let popupAddForm = document.querySelector('#popup__add-container');
+const editButton = document.querySelector('.profile__button-edit');
+const popupEdit = document.querySelector('.popup');
+const closeButtonAdd = document.getElementById('add_form_close');
+const closeButtonEdit = document.getElementById('edit_form_close');
+const addButton = document.querySelector('.profile__button-add');
+const popupEditForm = document.querySelector('#popup__edit-container');
+const popupAddForm = document.querySelector('#popup__add-container');
+
+function editFormValue(){
+nameInput.value = nameText.textContent;
+jobInput.value = jobText.textContent;
+} ;
 
 //открытие модального окна редактирования профиля
 editButton.addEventListener('click', function () {
+  editFormValue();
   popupEdit.classList.add('popup_opened');
   popupEditForm.classList.remove('popup__container_type_edit');
 });
@@ -31,13 +37,13 @@ closeButtonEdit.addEventListener('click', closeForm);
 //редактирование информации имени и работы
 const submitButtonEdit = document.getElementById('form__button_edit');
 const editForm = document.querySelector('#edit__form');
+const nameInput = document.querySelector('.form__text_type_name');
+const jobInput = document.querySelector('.form__text_type_activity');
+let nameText = document.querySelector('.profile__name');
+let jobText = document.querySelector('.profile__activity');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  const nameInput = document.querySelector('.form__text_type_name');
-  const jobInput = document.querySelector('.form__text_type_activity');
-  let nameText = document.querySelector('.profile__name');
-  let jobText = document.querySelector('.profile__activity');
   nameText.textContent = nameInput.value;
   jobText.textContent = jobInput.value;
   closeForm();
@@ -59,6 +65,12 @@ function addCards(event) {
   placeElement.querySelector('.place__name').textContent = placeName.value;
   placeElement.querySelector('.place__image').src = placeUrl.value;
   placeContainer.prepend(placeElement);
+  placeElement.querySelector('.place__button').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('place__button_active');
+ });
+ placeElement.querySelector('.place__trash').addEventListener('click', function (evt) {
+  evt.target.parentElement.remove();
+});
   closeForm();
 };
 
@@ -98,3 +110,22 @@ initialCards.forEach(function (element) {
   placeContainer.append(placeCard)
 });
 
+//возможность ставить лайки
+const placeButton = document.querySelectorAll('.place__button');
+const placeButtonArray = Array.from(placeButton);
+
+placeButtonArray.forEach(function(like) {
+  like.addEventListener('click' , function(){
+    like.classList.toggle('place__button_active');
+  });
+})
+
+//удаление карточек
+const placeCard = Array.from(document.querySelectorAll('.place'));
+const trashButton = Array.from(document.querySelectorAll('.place__trash'));
+
+trashButton.forEach(function(item) {
+  item.addEventListener('click' , function(){
+    item.parentElement.remove();
+    });
+  })
