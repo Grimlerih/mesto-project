c32ebaaf413ff3cc0a8e7c6dceb7a9fa7bd87700
Grimlerih <61974670+Grimlerih@users.noreply.github.com>
2022-06-25 20:08:02 +0300

    итерация №2

--- scripts/script.js
+++ scripts/script.js
@@ -1,154 +1,118 @@
 const editButton = document.querySelector('.profile__button-edit');
 const popupEdit = document.querySelector('.popup');
 const addButton = document.querySelector('.profile__button-add');
-const popupEditForm = document.querySelector('#popup__edit-container');
-const popupAddForm = document.querySelector('#popup__add-container');
+const popupEditForm = document.querySelector('#popup_edit-profile');
+const popupAddForm = document.querySelector('#popup_add-card');
+const imagePopup = document.querySelector('#popup_zoom-image');
 
-function editFormValue(){
-nameInput.value = nameText.textContent;
-jobInput.value = jobText.textContent;
-} ;
+function openPopup(popup) {
+  popupEdit.classList.add('popup_opened');
+  popup.classList.remove('popup__container_active');
+}
+
+function closePopup() {
+  popupEdit.classList.remove('popup_opened');
+  popupAddForm.classList.add('popup__container_active');
+  popupEditForm.classList.add('popup__container_active');
+  imagePopup.classList.add('popup__container_active');
+}
+
+//сравнивание значений поля ввода
+function editFormValue() {
+  nameInput.value = nameText.textContent;
+  jobInput.value = jobText.textContent;
+};
 
 //открытие модального окна редактирования профиля
 editButton.addEventListener('click', function () {
   editFormValue();
-  popupEdit.classList.add('popup_opened');
-  popupEditForm.classList.remove('popup__container_type_edit');
+  openPopup(popupEditForm);
 });
 
 //открытие модального окна добавления карточки
-addButton.addEventListener('click', function () {
-  popupEdit.classList.add('popup_opened');
-  popupAddForm.classList.remove('popup__container_type_add');
-});
+addButton.addEventListener('click', function(){
+  openPopup(popupAddForm);
+} );
 
-//закрытие модального окна добавления карточки и редактирования профиля
-const closeBut = Array.from(document.querySelectorAll('.popup__button-close')); //closeForm
-closeBut.forEach(function(item) {
-  item.addEventListener('click' , function(){
-    closeForm ();
+//закрытие модального окна
+const closeBut = Array.from(document.querySelectorAll('.popup__button-close'));
+closeBut.forEach(function (item) {
+  item.addEventListener('click', function () {
+    closePopup();
   })
 })
-
-function closeForm () {
-  popupEdit.classList.remove('popup_opened');
-  popupAddForm.classList.add('popup__container_type_add');
-  popupEditForm.classList.add('popup__container_type_edit');
-  imageContainer.classList.add('image-container_active');
-}
 
 //редактирование информации имени и работы
 const editForm = document.querySelector('#edit__form');
 const nameInput = document.querySelector('.form__text_type_name');
 const jobInput = document.querySelector('.form__text_type_activity');
-let nameText = document.querySelector('.profile__name');
-let jobText = document.querySelector('.profile__activity');
+const nameText = document.querySelector('.profile__name');
+const jobText = document.querySelector('.profile__activity');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   nameText.textContent = nameInput.value;
   jobText.textContent = jobInput.value;
-  closeForm();
+  closePopup();
 };
 
 editForm.addEventListener('submit', formSubmitHandler);
 
-//добавление карточек c помощью модального окна методом template
-const submitButtonAdd = document.querySelector('#form__button_add');
 const placeContainer = document.querySelector('.elements');
 const addForm = document.querySelector('#add__form');
-const placeName = document.querySelector('.form__text_place-name');
-const placeUrl = document.querySelector('.form__url');
 const placeTemplate = document.querySelector('#place').content;
 
-function addCards(event) {
-  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+//добавление карточек
+addForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  placeElement.querySelector('.place__name').textContent = placeName.value;
-  placeElement.querySelector('.place__image').src = placeUrl.value;
-  placeContainer.prepend(placeElement);
-  placeElement.querySelector('.place__button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('place__button_active');
- });
- placeElement.querySelector('.place__trash').addEventListener('click', function (evt) {
-  evt.target.parentElement.remove();
+
+  const placeName = document.querySelector('.form__text_place-name');
+  const placeUrl = document.querySelector('.form__url');
+
+  createCard(placeName.value, placeUrl.value);
+
+  closePopup();
+
+  placeName.value = '';
+  placeUrl.value = '';
 });
 
-placeElement.querySelector('.place__image').addEventListener('click' , function(){
-  popupEdit.classList.add('popup_opened');
-  imageContainer.classList.remove('image-container_active');
-  image.src = placeElement.querySelector('.place__image').src;
-  text.textContent = placeElement.querySelector('.place__name').textContent
-})
 
-  closeForm();
-};
+function createCard(name, link) {
+  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+  const placeButton = placeElement.querySelector('.place__button');
+  const placeTrash = placeElement.querySelector('.place__trash');
 
-addForm.addEventListener('submit', addCards);
+  placeElement.querySelector('.place__name').textContent = name;
+  placeElement.querySelector('.place__image').src = link;
 
-//добавление карточек с помощью js
-const initialCards = [{
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+  placeContainer.prepend(placeElement);
+
+  //открытие попапа с изображением
+  placeElement.querySelector('.place__image').addEventListener('click' , function(){
+    openPopup(imagePopup);
+    image.src = placeElement.querySelector('.place__image').src;
+    text.textContent = placeElement.querySelector('.place__name').textContent;
+  })
+
+  //лайк
+  placeElement.querySelector('.place__button').addEventListener('click' , function() {
+    placeButton.classList.toggle('place__button_active');
+  })
+
+  //удаление карточки
+  placeElement.querySelector('.place__trash').addEventListener('click' , function() {
+    placeTrash.parentElement.remove();
+  })
+
+  return placeElement;
+}
 
 const text = document.querySelector('.image-container__text');
 const imageContainer = document.querySelector('.image-container');
 const image = document.querySelector('.image-container__image');
 
-
+//создание карточек
 initialCards.forEach(function (element) {
-  const placeCard = placeTemplate.querySelector('.place').cloneNode(true);
-  placeCard.querySelector('.place__name').textContent = element.name;
-  placeCard.querySelector('.place__image').src = element.link;
-  placeContainer.append(placeCard)
-
-  placeCard.querySelector('.place__image').addEventListener('click' , function(){
-    popupEdit.classList.add('popup_opened');
-    imageContainer.classList.remove('image-container_active');
-    image.src = placeCard.querySelector('.place__image').src;
-    text.textContent = placeCard.querySelector('.place__name').textContent
-  })
+  createCard(element.name, element.link)
 });
-
-
-//возможность ставить лайки
-const placeButton = document.querySelectorAll('.place__button');
-const placeButtonArray = Array.from(placeButton);
-
-placeButtonArray.forEach(function(like) {
-  like.addEventListener('click' , function(){
-    like.classList.toggle('place__button_active');
-  });
-})
-
-//удаление карточек
-const placeCard = Array.from(document.querySelectorAll('.place'));
-const trashButton = Array.from(document.querySelectorAll('.place__trash'));
-
-trashButton.forEach(function(item) {
-  item.addEventListener('click' , function(){
-    item.parentElement.remove();
-    });
-  });
